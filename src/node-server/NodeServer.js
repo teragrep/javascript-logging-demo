3da8407cbe9e016a7e04ea3cdfb2117fd7a97258
spidermanja <38@teragrep.com>
2022-07-28 10:30:25 +0300

    Add the relp connection using RLP_02

--- src/node-server/NodeServer.js
+++ src/node-server/NodeServer.js
@@ -4,7 +4,12 @@ const Facility = require('@teragrep/rlo_08/src/main/js/Facility')
 const Severity = require('@teragrep/rlo_08/src/main/js/Severity')
 const SDElement = require('@teragrep/rlo_08/src/main/js/SDElement')
 const SDParam = require('@teragrep/rlo_08/src/main/js/SDParam')
-const UserAgent = require('user-agents')
+//const {RelpBatch, RelpConnection, RelpRequest, RelpWindow} = require("@teragrep/rlp_02");
+const RelpConnection = require('@teragrep/rlp_02/src/main/js/RelpConnection')
+const RelpBatch = require('@teragrep/rlp_02/src/main/js/RelpBatch')
+const async = require('async')
+
+
 
 
 const requestListener = function (req, res) {
@@ -38,8 +43,9 @@ const requestListener = function (req, res) {
 
       async function load() {
         const userAgent = req.headers['user-agent']
-        console.log(userAgent.toString());
-        
+        const ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddres || req.socket.remoteAddress || '';
+
+              
         // Set response header
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
         const dateTimestamp = '2014-07-24T17:57:36+03:00';
@@ -48,25 +54,26 @@ const requestListener = function (req, res) {
         let message = new SyslogMessage.Builder()
           .withAppName('bulk-data-sorted') //valid
        // .withTimestamp(timestamp) // In case if the timestamp disabled, it will go with system timestamp.
-         .withHostname('iris.teragrep.com') //valid
+          .withHostname(ip) //valid
           .withFacility(Facility.LOCAL0)
           .withSeverity(Severity.INFORMATIONAL)
-          .withProcId('8740') //validatied for the PRINTUSASCII format
+          .withProcId('8740') 
           .withMsgId('ID47')
           .withMsg(userAgent) // Fixed
           .withSDElement(new SDElement("exampleSDID@32473", new SDParam("iut", "3"), new SDParam("eventSource", "Application"))) // Fix the space before the previous 
+          .withDebug(true)
           .build()
 
-        console.log(message)
     
 
         let rfc5424message;
         rfc5424message = await message.toRfc5424SyslogMessage();
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(rfc5424message.toString(), 'utf-8', () => {
-        console.log('Writing ', rfc5424message.toString());
+       // console.log('Writing ', rfc5424message.toString());
+        sendMessage(rfc5424message)
        });
-         res.end(); //end the response
+        res.end(); //end the response
       }
       load();        
     }
@@ -74,3 +81,68 @@ const requestListener = function (req, res) {
         res.end('Invalid Request!'); //end the response   
 };
 module.exports = requestListener;
+
+function sendMessage(msg){
+
+  let relpConnection = new RelpConnection();
+  let host = '127.0.0.1'; 
+  let rlp_03_Port = 1601; // Tested with Docker CFE
+  
+  
+  async.waterfall(
+    [
+		function init(setConnect) {
+            setConnect(null, rlp_03_Port, host)
+        },
+		connect,
+    commit,
+    disconnect
+    ],
+  
+    function (err) {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            console.log('No Error')
+        }
+    }
+  );
+  async function connect() {
+    let conn = await relpConnection.connect(rlp_03_Port, host);	
+    console.log('Connectig...',host,' at PORT ', rlp_03_Port)
+    return conn;
+  }
+  async function disconnect(state) {
+    if(state){
+      await relpConnection.disconnect();
+      }
+    else {
+    console.log('Check the connection...')
+    }
+  }
+  
+  async function commit(){
+    return new Promise(async(resolve, reject) => {
+      let relpBatch = new RelpBatch();
+      relpBatch.insert(msg);
+      
+      let resWindow = await relpConnection.commit(relpBatch);
+      console.log('After Batch-1 Completion....', resWindow)
+      
+      let notSent = (resWindow === true) ? true : false; //Test purpose 
+      while(notSent){                          
+        let res = await relpBatch.verifyTransactionAllPromise();                              
+        if(res){
+            notSent = false;
+            console.log('VerifyTransactionAllPromise......', res);
+            resolve(true);
+            }
+        else{
+          reject(false);
+            }                            
+      }    
+     return resolve(true);
+    }) 
+  }
+}
