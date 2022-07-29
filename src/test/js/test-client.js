970bb064d070f73b28f278d50313e61699cdbf5f
spidermanja <38@teragrep.com>
2022-07-29 11:40:05 +0300

    Fix for the test client

---
+++ src/test/js/test-client.js
@@ -0,0 +1,32 @@
+const requestListener = require('../../node-server/NodeServer');
+const http = require('http')
+
+const host = 'localhost';
+const port = 3000;
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
+
+const options = {
+    hostname: 'localhost',
+    port: '3000',
+    path: '/ua',
+    method: 'GET'
+}
+
+const req = http.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on('data', (data) => {
+        process.stdout.write(data)
+    });
+});
+
+req.setHeader('user-agent', ['type=iris-teragrep', 'language=javascript']);
+req.on('error', (err) => {
+    console.error(err);
+})
+req.end();
