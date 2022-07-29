476f83e3a8c066e4a6319758c076563332ac50f3
spidermanja <38@teragrep.com>
2022-07-29 11:45:08 +0300

    Fix for build

--- src/test/js/test-server.js
+++ src/test/js/test-server.js
@@ -1,10 +1,15 @@
 const requestListener = require('../../node-server/NodeServer');
 const http = require('http')
-
+/**
+ * Lets disable this server and integrated with the client call instead
+ */
+/*
 const host = 'localhost';
 const port = 3000;
 
+ 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
+*/
