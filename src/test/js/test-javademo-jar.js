3a5d9b9350e0cc5fcd639e9fc93b27d1456b10b2
spidermanja <38@teragrep.com>
2022-08-05 11:10:56 +0300

    Fix for the workflow

--- src/test/js/test-javademo-jar.js
+++ src/test/js/test-javademo-jar.js
@@ -1,5 +1,7 @@
 const { exec } = require('child_process')
-
+/**
+ * @toddo This is not an ideal approach, for an experimental purpose
+ */
 exec('java -jar ../../src/java-relp-server-demo/java-relp-server-demo-jar-with-dependencies.jar', (error, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
